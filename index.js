b81c85ff6325567cc6e7fb59a2908ee9c15e4b13
AllaOz <ozcanalla@gmail.com>
2023-02-18 21:13:34 +0300

    added index.html, index.js,style.css

---
+++ index.js
@@ -0,0 +1,11 @@
+function sayHello() {
+    console.log("Я учу JavaScript!");
+  } 
+  sayHello();
+
+  function get1pic(){
+  let prevPic = document.getElementById('cat1');
+  prevPic.src ='./images/cat1.jpg';}
+  function get2pic(){
+  let nextPic = document.getElementById('cat1')
+  nextPic.src ='./images/cat2.jpg';}
