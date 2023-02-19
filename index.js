6b974cce5bc594cbfb4491a174e027a3885c5b85
AllaOz <ozcanalla@gmail.com>
2023-02-19 13:57:08 +0300

    added problem 2

--- index.js
+++ index.js
@@ -9,3 +9,25 @@ function sayHello() {
   function get2pic(){
   let nextPic = document.getElementById('cat1')
   nextPic.src ='./images/cat2.jpg';}
+  
+  function ifLeapYear(year){
+    if (year % 4 === 0) {
+     if (year % 100 === 0){
+     if (year % 400 === 0){
+       return true; 
+          } else {
+            return false;
+          }
+        } else {
+          return true;
+        }
+      } else {
+        return false;
+      }
+    }
+    let year = 2026;
+    if (ifLeapYear(year)) {
+      console.log(year + " год - Високосный год");
+    } else {
+      console.log(year + " год - Невисокосный год");
+    }
